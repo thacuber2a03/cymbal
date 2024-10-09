7d0b540cf5e94227e36a8cf6182411e428db1e35
thacuber2a03 <70547062+thacuber2a03@users.noreply.github.com>
2024-10-09 15:49:55 -0400

    refactor!: removed UTF-8 support for lexer
    there wasn't really much use in a system not exactly designed with support for it

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,47 +4,39 @@ import (
 	"fmt"
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
 
 type Lexer struct {
-	Source []byte
+	Source string
 	index  int
 	line   uint
 }
 
 func New(source string) *Lexer {
 	return &Lexer{
-		Source: []byte(source),
+		Source: source,
 		line:   1,
 	}
 }
 
-func (l *Lexer) peekCharSize() (rune, int) {
-	ch, sz := utf8.DecodeRune(l.Source[l.index:])
-	if ch == utf8.RuneError && sz == 0 {
-		return 0, 0
+func (l *Lexer) atEnd() bool { return l.index >= len(l.Source) }
+
+func (l *Lexer) peekChar() byte {
+	if l.atEnd() {
+		return 0
 	}
-	return ch, sz
+	return l.Source[l.index]
 }
 
-func (l *Lexer) peekChar() rune {
-	ch, _ := l.peekCharSize()
-	return ch
-}
+func (l *Lexer) nextChar() byte {
+	ch := l.peekChar()
+	l.index++
 
-func (l *Lexer) nextChar() (ch rune) {
-	ch, sz := l.peekCharSize()
-	if ch == utf8.RuneError {
-		return
-	} else if ch == '\n' {
+	if ch == '\n' {
 		l.line++
 	}
-	l.index += sz
-	return
+	return ch
 }
-
-func (l *Lexer) atEnd() bool { return l.index >= len(l.Source) }
 
 func (l *Lexer) token(tt TokenType, lexeme string) Token {
 	return Token{Type: tt, Lexeme: lexeme, Line: l.line}
@@ -54,7 +46,9 @@ func (l *Lexer) error(msg string, args ...any) Token {
 	return l.token(TT_ERROR, fmt.Sprintf(msg, args...))
 }
 
-func (l *Lexer) invalidRune(r rune) Token { return l.error("invalid UTF-8 rune %U", r) }
+func isValidIdent(ch byte) bool {
+	return unicode.IsLetter(rune(ch)) || ch == '_'
+}
 
 var keywords = map[string]TokenType{
 	"fn":  TT_FN,
@@ -64,22 +58,9 @@ var keywords = map[string]TokenType{
 func (l *Lexer) ident() Token {
 	var id strings.Builder
 
-	for {
-		if l.atEnd() {
-			break
-		}
-
-		c := l.peekChar()
-		if c == utf8.RuneError {
-			return l.invalidRune(c)
-		}
-
-		if unicode.IsLetter(c) || unicode.IsDigit(c) {
-			id.WriteRune(c)
-			l.nextChar()
-		} else {
-			break
-		}
+	for !l.atEnd() && isValidIdent(l.peekChar()) ||
+		unicode.IsDigit(rune(l.peekChar())) {
+		id.WriteByte(l.nextChar())
 	}
 
 	if id.Len() == 0 {
@@ -93,97 +74,67 @@ func (l *Lexer) ident() Token {
 	return l.token(TT_IDENT, str)
 }
 
-func isValidDigit(r rune) bool { return '0' <= r && r <= '9' }
-
-var validBases = map[rune]struct {
-	f    func(r rune) bool
+var validBases = map[byte]struct {
+	fn   func(r byte) bool
 	name string
 }{
 	'x': {
-		f:    func(r rune) bool { l := unicode.ToLower(r); return isValidDigit(l) || 'a' <= l && l <= 'f' },
+		fn:   func(ch byte) bool { return unicode.Is(unicode.ASCII_Hex_Digit, rune(ch)) },
 		name: "hexadecimal",
 	},
 	'b': {
-		f:    func(r rune) bool { return r == '0' || r == '1' },
+		fn:   func(ch byte) bool { return ch == '0' || ch == '1' },
 		name: "binary",
 	},
 }
 
 func (l *Lexer) number() Token {
-	// TODO(thacuber2a03): this and ident() use really similar code
 	var num strings.Builder
 
-	for {
-		if l.atEnd() {
-			break
-		}
-
-		c := l.peekChar()
-		if c == utf8.RuneError {
-			return l.invalidRune(c)
-		}
-
-		if isValidDigit(c) {
-			num.WriteRune(c)
-			l.nextChar()
-		} else {
-			break
-		}
+	for !l.atEnd() && unicode.IsDigit(rune(l.peekChar())) {
+		num.WriteByte(l.nextChar())
 	}
 
 	if num.Len() == 0 {
 		panic("(*Lexer).number was called but no integer literal was scanned")
 	}
 
-	if l.atEnd() {
-		goto end
-	}
+	if b, ok := validBases[l.peekChar()]; !l.atEnd() && ok {
+		num.WriteByte(l.nextChar())
 
-	if b, ok := validBases[l.peekChar()]; ok {
-		num.WriteRune(l.nextChar())
-		scanned := false
+		var res strings.Builder
 
-		for {
-			if l.atEnd() {
-				break
-			}
-
-			c := l.peekChar()
-			if c == utf8.RuneError {
-				return l.invalidRune(c)
-			}
-
-			if b.f(c) {
-				num.WriteRune(c)
-				scanned = true
-				l.nextChar()
-			} else {
-				break
-			}
+		for !l.atEnd() && b.fn(l.peekChar()) {
+			res.WriteByte(l.nextChar())
 		}
 
-		if !scanned {
+		if res.Len() == 0 {
 			return l.error("no digits in %s literal", b.name)
 		}
+
+		num.WriteString(res.String())
 	}
 
-end:
 	return l.token(TT_INT, num.String())
 }
 
 func (l *Lexer) charLit() Token {
 	var lit strings.Builder
-	lit.WriteRune(l.nextChar())
-	if l.atEnd() { return l.error("unterminated character literal") }
+	lit.WriteByte(l.nextChar())
+	if l.atEnd() {
+		return l.error("unterminated character literal")
+	}
 
 	if c := l.nextChar(); c == '\\' {
 
 	} else {
-		lit.WriteRune(c)
+		lit.WriteByte(c)
 	}
 
-	if l.atEnd() { return l.error("unterminated character literal") }
-	lit.WriteRune(l.nextChar())
+	if l.atEnd() {
+		return l.error("unterminated character literal")
+	}
+	lit.WriteByte(l.nextChar())
 	return l.token(TT_CHAR, lit.String())
 }
 
@@ -199,9 +150,6 @@ func (l *Lexer) Next() Token {
 	c := l.peekChar()
 
 	switch c {
-	case utf8.RuneError:
-		l.nextChar()
-		return l.error("invalid UTF-8 rune")
 	case '{':
 		l.nextChar()
 		return l.token(TT_LBRACE, "{")
@@ -215,9 +163,9 @@ func (l *Lexer) Next() Token {
 		return l.charLit()
 	}
 
-	if unicode.IsLetter(c) {
+	if isValidIdent(c) {
 		return l.ident()
-	} else if unicode.IsDigit(c) {
+	} else if unicode.IsDigit(rune(c)) {
 		return l.number()
 	}
 
